600f759042f2476b56a006907c7d03d6909d7ab1
fikrilal <fikrimuzakky24@gmail.com>
2026-01-27 22:29:11 +0700

    feat(profile-image): implement use case and entity for clearing profile images

---
+++ lib/features/user/domain/usecase/clear_profile_image_usecase.dart
@@ -0,0 +1,25 @@
+import 'package:fpdart/fpdart.dart';
+
+import 'package:mobile_core_kit/core/user/entity/user_entity.dart';
+import 'package:mobile_core_kit/features/auth/domain/failure/auth_failure.dart';
+import 'package:mobile_core_kit/features/user/domain/entity/clear_profile_image_request_entity.dart';
+import 'package:mobile_core_kit/features/user/domain/repository/profile_image_repository.dart';
+import 'package:mobile_core_kit/features/user/domain/usecase/get_me_usecase.dart';
+
+class ClearProfileImageUseCase {
+  ClearProfileImageUseCase(this._profileImageRepository, this._getMe);
+
+  final ProfileImageRepository _profileImageRepository;
+  final GetMeUseCase _getMe;
+
+  Future<Either<AuthFailure, UserEntity>> call(
+    ClearProfileImageRequestEntity request,
+  ) async {
+    final result = await _profileImageRepository.clearProfileImage(request);
+
+    return result.match(
+      (failure) => Future.value(left(failure)),
+      (_) => _getMe(),
+    );
+  }
+}
